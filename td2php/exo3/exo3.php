0dc4dd9166637cc97c075c662fa837c6e270f03d
burguet <lucasburguet22020@gmail.com>
2023-01-18 17:18:38 +0100

    z

--- td2php/exo3/exo3.php
+++ td2php/exo3/exo3.php
@@ -1,3 +1,14 @@
+<?php
+function Moyenne ($tableau) {
+    $somme=0;
+    for ($i=0; $i<sizeof($tableau); $i++){
+        $somme += $tableau[$i];
+    }
+    $moyenne=$somme/sizeof($tableau);
+    return $moyenne;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +19,12 @@
 </head>
 <body>
     <?php
-    
+$t=[28, 56, 98, 45, 48];
+for ($i=0; $i<sizeof($t); $i++){
+    echo"<p>".$t[$i]."</p>";
+}
+
+echo "<p> la moyenne est :".Moyenne($t)."</p>";
     ?>
 </body>
 </html>
