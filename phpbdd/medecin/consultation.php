cac9df7eaf6d514f01faf36bbd3886b12940cc14
burguet <lucasburguet22020@gmail.com>
2023-01-27 16:02:50 +0100

    kl

--- phpbdd/medecin/consultation.php
+++ phpbdd/medecin/consultation.php
@@ -8,57 +8,57 @@
 </head>
 <body>
 <?php echo "<h1>coucou je suis pres et vous ?</h1>";
-
+if (isset($_POST["Valider"]))
+{
+    echo "idMedecin = ".$_POST["idMedecin"]." id patient = ".$_POST["idPatient"]." date = ".$_POST["laDate"];
+}
 try {
 
-    $ipserver ="192.168.65.193";
-    $nomBase = "Medecin";
+    $ipserver ="192.168.65.167";
+    $nomBase = "medecin";
     $loginPrivilege ="SiteWeb";
     $passPrivilege ="SiteWeb";
 
     $GLOBALS["pdo"] = new PDO('mysql:host=' . $ipserver . ';dbname=' . $nomBase . '', $loginPrivilege, $passPrivilege);
 
     $requete = "select * from Medecin";
+    $requete2 = "select * from Patient";
     $resultat = $GLOBALS["pdo"]->query($requete);
-    //resultat est du coup un objet de type PDOStatement
-    $tabMedecins = $resultat->fetchALL();
-   
+    $resultat2 = $GLOBALS["pdo"]->query($requete2);
+$tabMedecins = $resultat->fetchALL();
+$tabPatient = $resultat2->fetchALL();
     ?>
     <form action="" method="post">
         <select name="idMedecin">
             <?php
             foreach ($tabMedecins as $Medecin) {
                 ?>
-                <option value="<?=$Medecin["id"]?>"><?=$Medecin["nom"]." ".$Medecin["prenom"]?>s</option>
+                <option value="<?=$Medecin["id"]?>"><?=$Medecin["nom"]." ".$Medecin["prenom"]?></option>
                 <?php
             }
           
             ?>
         </select>
-    </form>
-    <?php
-
-     $requete2 = "select * from Patient";
-     $resultat2 = $GLOBALS["pdo"]->query($requete2);
-     $tabPatient = $resultat2->fetchALL();
-
-    ?>
-    <form action="" method="post">
         <select name="idPatient">
             <?php
            
             foreach ($tabPatient as $Patient) {
                 ?>
-                <option value="<?=$Patient["id"]?>"><?=$Patient["nom"]." ".$Patient["prenom"]?>s</option>
+                <option value="<?=$Patient["id"]?>"><?=$Patient["nom"]." ".$Patient["prenom"]?></option>
                 <?php
             }
             ?>
         </select>
+        <input type="datetime-local" name="laDate">
+        <input type="submit" value="Saisir une consultation" name="Valider">
     </form>
-   
-    
-<?php
-} catch (Exception  $error) {
+   <?php
+        $requete3 = "INSERT INTO `Consultation`( `Dateheure`, `idMedecin`, `idPatient`) VALUES ('" . $_POST['laDate'] . "','" . $_POST['idMedecin'] . "','" . $_POST['idPatient'] . "')";
+        $resultat3 = $GLOBALS["pdo"]->query($requete3);
+
+
+}
+ catch (Exception  $error) {
     echo "error est : ".$error->getMessage();
 }
 ?>
