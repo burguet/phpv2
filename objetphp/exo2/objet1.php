c8e0cb568394822353761235cd11eb729008fc32
burguet <lucasburguet22020@gmail.com>
2023-02-01 15:31:44 +0100

    j

---
+++ objetphp/exo2/objet1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    class Personnage 
+    {
+        public $Pseudo;
+        public $Vie;
+      
+        public function __construct() 
+        {
+            echo "bonjour";
+             $this->Vie = 100;
+        }
+      }
+      
+    ?>
+</body>
+</html>
