a693220b7de285ffc54f5a29d5568d4595d7192b
burguet <lucasburguet22020@gmail.com>
2023-02-01 14:39:10 +0100

    corection

---
+++ objetphp/exo1/objet.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+class utilisateur 
+{
+    private $Noms;
+    private $prenom;
+
+    
+    public function seconnecter()
+    {
+        ?>
+        <table border =1px id=tableau>
+            <tr>
+                <th>utilisateur</th>
+            </tr>
+            <tr>
+                <td><p>-Nom : String</p>
+                    <p>-Prenom : String</p></td>
+            </tr>
+            <tr>
+                <td>+seconnecter() : void</td>
+            </tr>
+
+        </table>
+        <?php     
+    }
+}
+?>  
+</body>
+</html>
