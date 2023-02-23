ce0fad3585a59a1de8388a53eb4e69b5ade25123
burguet <burguetlucas@gmail.com>
2023-02-23 20:10:39 +0100

    exo5

--- objetphp/exo5/exo5.php
+++ objetphp/exo5/exo5.php
@@ -4,12 +4,23 @@
     <meta charset="UTF-8">
     <?php require("objet5.php"); ?>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
     <?php
-   
-    ?>
+    $personnage1 = new Personnage("1");
+    $personnage2 = new Personnage("2");
+    
+    echo "<p>Avant l'attaque :</p>";
+    $personnage1->afficherInfos();
+    $personnage2->afficherInfos();
+    
+    $personnage1->attaquer($personnage2);
+    
+    echo "<p> Après l'attaque : </p>";
+    $personnage1->afficherInfos();
+    $personnage2->afficherInfos();
+   ?>
 </body>
 </html>
--- objetphp/exo5/objet5.php
+++ objetphp/exo5/objet5.php
@@ -1,4 +1,54 @@
 <?php
 
+class Personnage {
+    private $vie;
+    private $pseudo;
+    
 
+    public function __construct($id) 
+    {
+       
+        $my_db=new PDO('mysql:host=192.168.1.28; dbname=objet; charset=utf8','root', 'root'); //permet d'inialiter la connection a la base
+        $data_perso = $my_db->query("SELECT nom, vie FROM `personnage` WHERE id=".$id.";");//récupération des résultat de la requête 
+        $t_perso = $data_perso->fetch(); // mise en forme des information dans un tableau grâce aux fetch
+        print_r($t_perso); // affichage du tableau de la ligne 13
+        $this->pseudo = $t_perso['nom']; //stockage du nom récupérer dans $pseudo
+        $this->vie = $t_perso['vie']; //stockage de la vie récupérer dans $vie
+        $data_perso ->closeCursor(); //fermeture de la connexion
+    }
+    
+    public function attaquer(Personnage $personnage) {
+        echo"<p>".$this->pseudo . " attaque " . $personnage->getPseudo() . "</p>";
+        $personnage->defense(50);
+    }
+    
+    public function defense($degats)
+    {
+        echo "<p>".$this->pseudo . " se défend contre une attaque de " . $degats . " dégâts</p>";
+
+        $this->vie -= $degats;
+
+        if ($this->vie <= 0)
+        {
+            echo "<p>".$this->pseudo . " est mort</p>";
+        } 
+
+        else {
+            echo "<p>".$this->pseudo . " a " . $this->vie . " points de vie restants </p>";
+        }
+    }
+    
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+    
+    public function afficherInfos()
+    {
+        echo "<p> Pseudo : " . $this->pseudo . ", Points de vie : " . $this->vie . "</p>";
+    }
+}
+
+
+    
 ?>
